7d88582aec01d01927a42458a70980d73f15249b
Dac Kinh Luan Hoang <j.hoang@spectrum8.de>
2024-12-18 22:40:21 +0100

    add: create App.vue with SearchBox and LinkCollection components

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import { Icon } from '@iconify/vue'
+import LinkCollection from '../components/LinkCollection.vue'
+import SearchBox from '../components/SearchBox.vue'
+</script>
+
+<template>
+  <div class="w-screen h-screen grid place-content-center">
+    <div>
+      <SearchBox />
+      <LinkCollection />
+    </div>
+  </div>
+</template>
