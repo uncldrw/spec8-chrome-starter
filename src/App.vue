f23ee1e2f8f24d60b740792cebeb48412a5b2bbf
uncldrw <dreq2905@gmail.com>
2024-12-21 21:07:44 +0100

    style: implement CSS Houdini squircle support in App.vue with polyfill fallback

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
 // import { Icon } from '@iconify/vue'
+import { onMounted } from 'vue'
 import LinkCollection from '../components/LinkCollection.vue'
 import SearchBox from '../components/SearchBox.vue'
+import 'css-houdini-squircle'
+
+onMounted(async () => {
+  if (!CSS.paintWorklet) {
+    await import('css-paint-polyfill')
+  }
+  await CSS.paintWorklet.addModule('https://unpkg.com/css-houdini-squircle/squircle.min.js')
+})
 </script>
 
 <template>
@@ -10,5 +19,10 @@ import SearchBox from '../components/SearchBox.vue'
       <SearchBox />
       <LinkCollection />
     </div>
+    <span></span>
+    <span></span>
+    <span></span>
   </div>
 </template>
+
+<script></script>
